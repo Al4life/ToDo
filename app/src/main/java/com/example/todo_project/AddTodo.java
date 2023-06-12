dbd492a086b4773752aa26b238099e48d139aa50
Richard <tessbry814@gmail.com>
2023-06-12 07:44:41 +0000

    Initial commit

---
+++ app/src/main/java/com/example/todo_project/AddTodo.java
@@ -0,0 +1,18 @@
+package com.example.todo_project;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class AddTodo extends AppCompatActivity {
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.add_todo);
+        }
+
+    }
