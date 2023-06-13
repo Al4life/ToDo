8210fc541dfee11a153f64e8a8267f8428b64155
Richard <tessbry814@gmail.com>
2023-06-13 20:13:37 +0000

    Trying an implementation

--- app/src/main/java/com/example/todo_project/AddTodo.java
+++ app/src/main/java/com/example/todo_project/AddTodo.java
@@ -1,18 +1,38 @@
 package com.example.todo_project;
 
 import android.os.Bundle;
+import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+
 
 public class AddTodo extends AppCompatActivity {
 
-        @Override
+    public static String enteredText = "";
+
+
+    @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.add_todo);
+
+            //Set an onClick Listener for Create Todo button
+            Button add= findViewById(R.id.createTodo);
+
+            EditText editText=(EditText)findViewById(R.id.editText);
+
+            add.setOnClickListener(new View.OnClickListener(){
+                public void onClick(View v){
+                    enteredText= String.valueOf(editText);
+
+                }
+
+            });
+
         }
 
-    }
+}
--- app/src/main/java/com/example/todo_project/MainActivity.java
+++ app/src/main/java/com/example/todo_project/MainActivity.java
@@ -4,15 +4,22 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 
 public class MainActivity extends AppCompatActivity {
+    TextView textView=findViewById(R.id.textView);
+    String textToBeDisplayed= AddTodo.enteredText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView.setText(textToBeDisplayed);
+
+        
 
 
     }
