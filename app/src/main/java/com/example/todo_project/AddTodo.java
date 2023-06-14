5aa39c36e68efea75028ed0d4891374a89a7d4f9
Richard <tessbry814@gmail.com>
2023-06-14 19:46:53 +0000

    Well no one objected!

--- app/src/main/java/com/example/todo_project/AddTodo.java
+++ app/src/main/java/com/example/todo_project/AddTodo.java
@@ -1,38 +1,112 @@
 package com.example.todo_project;
 
-import android.os.Bundle;
-import android.widget.EditText;
-import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
+import android.widget.EditText;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class AddTodo extends AppCompatActivity {
-
-    public static String enteredText = "";
-
+    private EditText editDesc;
+    String display="ToDo Added!";
 
     @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.add_todo);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_todo);
 
-            //Set an onClick Listener for Create Todo button
-            Button add= findViewById(R.id.createTodo);
+       // editTitle = findViewById(R.id.editText);
+        editDesc = findViewById(R.id.editDesc);
 
-            EditText editText=(EditText)findViewById(R.id.editText);
+        Button add = findViewById(R.id.createTodo);
 
-            add.setOnClickListener(new View.OnClickListener(){
-                public void onClick(View v){
-                    enteredText= String.valueOf(editText);
+        Button delFirst=findViewById(R.id.button);
+        Button delSecond=findViewById(R.id.button2);
+        Button delThird=findViewById(R.id.button3);
 
-                }
 
-            });
+        delFirst.setBackgroundColor(getResources().getColor(R.color.red));
+        delSecond.setBackgroundColor(getResources().getColor(R.color.red));
+        delThird.setBackgroundColor(getResources().getColor(R.color.red));;
 
-        }
 
+
+
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String enteredDesc = editDesc.getText().toString();
+                Intent resultIntent = new Intent();
+                resultIntent.putExtra("description", enteredDesc);
+                setResult(RESULT_OK, resultIntent);
+                finish();
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(AddTodo.this, display, duration);
+                toast.show();
+
+            }
+        });
+
+        delFirst.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String enteredDesc = "deleteFirst";
+                Intent resultIntent = new Intent();
+                resultIntent.putExtra("description", enteredDesc);
+                setResult(RESULT_OK, resultIntent);
+                finish();
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(AddTodo.this, "First ToDo Deleted!", duration);
+                toast.show();
+
+            }
+        });
+        delSecond.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String enteredDesc = "deleteSecond";
+                Intent resultIntent = new Intent();
+                resultIntent.putExtra("description", enteredDesc);
+                setResult(RESULT_OK, resultIntent);
+                finish();
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(AddTodo.this, "Second ToDo Deleted!", duration);
+                toast.show();
+
+            }
+        });
+        delThird.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String enteredDesc = "deleteThird";
+                Intent resultIntent = new Intent();
+                resultIntent.putExtra("description", enteredDesc);
+                setResult(RESULT_OK, resultIntent);
+                finish();
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(AddTodo.this, "Last ToDo Deleted!", duration);
+                toast.show();
+
+            }
+        });
+
+
+
+
+    }
+
+    public void returnBack(View view) {
+        finish();
+    }
 }
--- app/src/main/java/com/example/todo_project/MainActivity.java
+++ app/src/main/java/com/example/todo_project/MainActivity.java
@@ -1,31 +1,87 @@
 package com.example.todo_project;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
 
 import android.widget.TextView;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 
 public class MainActivity extends AppCompatActivity {
-    TextView textView=findViewById(R.id.textView);
-    String textToBeDisplayed= AddTodo.enteredText;
+    private static final int REQUEST_CODE_ADD_TODO = 1;
+    private TextView titleTextView;
+    private TextView descTextView;
+    private TextView descTextView2;
+    private TextView descTextView1;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textView.setText(textToBeDisplayed);
 
-        
+        descTextView = findViewById(R.id.textView);
+        descTextView1 = findViewById(R.id.textView2);
+        descTextView2 = findViewById(R.id.textView3);
+
+
+
+        descTextView.setTextSize(20);
+        descTextView.setTextColor(Color.BLACK);
+        descTextView.setTypeface(Typeface.defaultFromStyle(Typeface.BOLD));
+
+
+        descTextView1.setTextSize(20);
+        descTextView1.setTextColor(Color.BLACK);
+        descTextView1.setTypeface(Typeface.defaultFromStyle(Typeface.BOLD));
+
+        descTextView2.setTextSize(20);
+        descTextView2.setTextColor(Color.BLACK);
+        descTextView2.setTypeface(Typeface.defaultFromStyle(Typeface.BOLD));
 
 
     }
 
     public void openNewActivity(View view) {
         Intent intent = new Intent(this, AddTodo.class);
-        startActivity(intent);
+        startActivityForResult(intent, REQUEST_CODE_ADD_TODO);
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE_ADD_TODO && resultCode == RESULT_OK) {
+            String description = data.getStringExtra("description");
+            if (description.equals("deleteFirst")){
+                descTextView.setText("");
+
+            } else if (description.equals("deleteSecond")){
+                descTextView1.setText("");
+
+            } else if (description.equals("deleteThird")){
+                descTextView2.setText("");
+
+            }else{
+
+            if (descTextView.getText().toString().isEmpty()) {
+                descTextView.setText(description);
+            } else if (descTextView1.getText().toString().isEmpty()) {
+                descTextView1.setText(description);
+            } else if (descTextView2.getText().toString().isEmpty()) {
+                descTextView2.setText(description);
+            } else {
+                CharSequence text = "My Limit's exceeded, bruh!!";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(this /* MyActivity */, text, duration);
+                toast.show();
+            }
+        }}
+    }
+
 }
